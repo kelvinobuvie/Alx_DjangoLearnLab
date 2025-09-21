119b5beb802d49a6a434fd5b42f16f2bc965261d
kelvinobuvie <kelvinobuvie@gmail.com>
2025-09-21 20:36:21 +0100

    new

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,8 @@
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import DetailView, ListView
+from .models import Book, Library   # ✅ This is what the checker wants
+
+
 def list_books(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})

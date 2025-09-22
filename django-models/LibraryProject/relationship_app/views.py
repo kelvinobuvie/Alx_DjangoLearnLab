51dff751597ed60a338951f9b57952a2153d9bc5
kelvinobuvie <kelvinobuvie@gmail.com>
2025-09-22 03:56:14 +0100

    more

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import DetailView
 # ✅ The checker is looking for this exact import line
-from .models import Book, Library
+from .models import Library
+from .models import Book
+
 
 
 # ✅ Function-based view: List all books

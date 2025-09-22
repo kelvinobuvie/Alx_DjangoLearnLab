7cd96ec67e99cb929cf70715d3882d6df1f6834f
kelvinobuvie <kelvinobuvie@gmail.com>
2025-09-22 04:08:58 +0100

    new changes

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -4,7 +4,8 @@ from django.http import HttpResponseForbidden
 from .models import Library
 from .models import Book
 from django.views.generic.detail import DetailView
-
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 # ====================================================
 # ROLE-BASED ACCESS CONTROL VIEWS
 # ====================================================

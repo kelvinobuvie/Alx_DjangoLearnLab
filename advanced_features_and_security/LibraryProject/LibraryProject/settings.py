fadc1afaacf0a165600e5894a988e43a51e69964
kelvinobuvie <kelvinobuvie@gmail.com>
2025-09-23 03:52:41 +0100

    more

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -100,6 +100,22 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = 'bookshelf.CustomUser'
+
+DEBUG = False
+
+# HTTPS / Security
+SECURE_SSL_REDIRECT = True
+SECURE_HSTS_SECONDS = 31536000
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
+X_FRAME_OPTIONS = 'DENY'
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_BROWSER_XSS_FILTER = True
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/

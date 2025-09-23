b72240303a1fba9f93a39dfde247177d13f67cab
kelvinobuvie <kelvinobuvie@gmail.com>
2025-09-23 03:48:23 +0100

    new

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
-from .models import Book  # ✅ This is what the checker wants
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your model with the admin site
-@admin.register(Book)
-class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'publication_year')  # columns in the admin list view
-    search_fields = ('title', 'author')                     # allow searching by title/author
-    list_filter = ('publication_year',)                     # add a filter by publication year
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
